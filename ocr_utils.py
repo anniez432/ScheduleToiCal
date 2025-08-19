f5d8efe21945e6a270b8ec791fe44513615e4360
Annie <zhao15a@outlook.com>
2025-08-18 23:42:25 -0500

    Set-up files & env

---
+++ ocr_utils.py
@@ -0,0 +1,14 @@
+import cv2
+import pytesseract
+
+def preprocess_img(img_path: str) -> str:
+    img = cv2.imread(img_path)
+
+    grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayscale = cv2.threshold(
+        grayscale, 150, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
+    )[1]
+
+    text = pytesseract.image_to_string(grayscale)
+
+    return text
