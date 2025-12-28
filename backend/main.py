ffe65b252981dd6188c02eccea44c28603e4e202
Annie <zhao15a@outlook.com>
2025-12-28 15:07:29 -0600

    Update with render backend url

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://scheduletoical.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
