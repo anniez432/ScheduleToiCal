bd78f66d201789e0da735cf48f5d5780a3a34457
Annie <zhao15a@outlook.com>
2025-12-28 17:22:19 -0600

    Update server-side title

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,9 @@ import Home from "./Home";
 
 export const metadata = {
   title: "UW-Madison Schedule to iCal Converter",
+  openGraph: {
+    title: "UW-Madison Schedule to iCal Converter",
+  },
 };
 
 export default function Page() {
